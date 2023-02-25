c0742f45cac6d90977632c9aa3d9680167ed4eff
unknown <bencbchurchill@gmail.com>
2023-02-25 11:10:29 -0800

    First pass at training nueral network. Modified main to take an argument

--- java/src/main/java/edu/gatech/bchurchill/assignment2/Assignment.java
+++ java/src/main/java/edu/gatech/bchurchill/assignment2/Assignment.java
@@ -1,29 +1,55 @@
 package edu.gatech.bchurchill.assignment2;
 
 import edu.gatech.bchurchill.assignment2.part1.*;
+import edu.gatech.bchurchill.assignment2.part2.LetterDataSetReader;
+import edu.gatech.bchurchill.assignment2.part2.NeuralNetworkProblemSet;
+import shared.filt.DataSetFilter;
+import shared.filt.RandomOrderFilter;
 
 import java.util.List;
 
 public class Assignment {
 
-    public static void main(String[] args) {
-        List<OptimizationProblemSet> problems = List.of(
-            new NQueensProblemSet(8),
-            new KnapsackProblemSet(8),
-            new SixPeaksProblemSet(10),
-            new TravelingSalesmanProblemSet(8)
-        );
-
-        for(var problem: problems) {
-            System.out.println("Problem: " + problem.getName());
-            System.out.println("\tRandomized Hill Climbing");
-            System.out.println("\t\t"+problem.randomizedHillClimbing());
-            System.out.println("\tSimulated Annealing");
-            System.out.println("\t\t"+problem.simulatedAnnealing());
-            System.out.println("\tGenetic Algorithm");
-            System.out.println("\t\t"+problem.geneticAlgorithm());
-            System.out.println("\tMIMIC");
-            System.out.println("\t\t"+problem.mimic());
+    public static void main(String[] args) throws Exception {
+        String mode = null;
+        try {
+            mode = args[0];
+        } catch(ArrayIndexOutOfBoundsException e) {
+            printIncorrectUsageMessage();
         }
+
+        if(mode.equals("part1")) {
+            List<OptimizationProblemSet> problems = List.of(
+                    new NQueensProblemSet(8),
+                    new KnapsackProblemSet(8),
+                    new SixPeaksProblemSet(10),
+                    new TravelingSalesmanProblemSet(8)
+            );
+
+            for(var problem: problems) {
+                System.out.println("Problem: " + problem.getName());
+                System.out.println("\tRandomized Hill Climbing");
+                System.out.println("\t\t"+problem.randomizedHillClimbing());
+                System.out.println("\tSimulated Annealing");
+                System.out.println("\t\t"+problem.simulatedAnnealing());
+                System.out.println("\tGenetic Algorithm");
+                System.out.println("\t\t"+problem.geneticAlgorithm());
+                System.out.println("\tMIMIC");
+                System.out.println("\t\t"+problem.mimic());
+            }
+        } else if(mode.equals("part2")) {
+            List<DataSetFilter> filters = List.of(new RandomOrderFilter());
+            var dataset = new LetterDataSetReader(filters).read();
+            var problemSet = new NeuralNetworkProblemSet(dataset, 90, 1000);
+        } else {
+            printIncorrectUsageMessage();
+        }
+    }
+
+    private static void printIncorrectUsageMessage() {
+        System.err.println("Please select a mode from {part1, part2}.");
+        System.err.println("\tpart1: Randomized optimization algorithms over 3 problems");
+        System.err.println("\tpart2: Randomized optimization algorithms to train neural network");
+        System.exit(1);
     }
 }
--- java/src/main/java/edu/gatech/bchurchill/assignment2/part2/NeuralNetworkProblemSet.java
+++ java/src/main/java/edu/gatech/bchurchill/assignment2/part2/NeuralNetworkProblemSet.java
@@ -5,18 +5,30 @@ import edu.gatech.bchurchill.assignment2.part1.SolutionStatistics;
 import func.nn.OptNetworkBuilder;
 import func.nn.feedfwd.FeedForwardNetwork;
 import shared.DataSet;
+import shared.GradientErrorMeasure;
+import shared.Instance;
+import shared.SumOfSquaresError;
 import shared.filt.TestTrainSplitFilter;
 
+/**
+ * Docs used:
+ *  https://github.com/pushkar/ABAGAIL
+ *  https://github.com/pushkar/ABAGAIL/blob/master/src/opt/test/AbaloneTest.java
+ */
 public class NeuralNetworkProblemSet extends BaseProblemSet {
     private int outputLayerSize;
-    private DataSet data;
+    private DataSet trainingSet;
+    private DataSet testSet;
     private int percentTrain;
     private int iterations;
 
     public NeuralNetworkProblemSet(DataSet dataSet, int percentTrain, int iterations) {
-        this.data = dataSet;
+        TestTrainSplitFilter testTrainSplit = new TestTrainSplitFilter(percentTrain);
+        testTrainSplit.filter(dataSet);
+        this.trainingSet = testTrainSplit.getTrainingSet();
+        this.testSet = testTrainSplit.getTestingSet();
         this.percentTrain = percentTrain;
-        this.outputLayerSize = data.getLabelDataSet().getDescription().getAttributeCount();
+        this.outputLayerSize = dataSet.getLabelDataSet().getDescription().getAttributeCount();
         this.iterations = iterations;
     }
 
@@ -27,38 +39,62 @@ public class NeuralNetworkProblemSet extends BaseProblemSet {
 
     @Override
     public SolutionStatistics randomizedHillClimbing() {
-        FeedForwardNetwork network = baseBuilder()
-                .withRHC()
-                .train();
+        var network = baseBuilder()
+                .withRHC();
 
-        return null; // TODO get statistics
+        return solve(network);
     }
 
     @Override
     public SolutionStatistics simulatedAnnealing() {
-        return null;
+        var network = baseBuilder()
+                .withSA(1E11, .975);
+
+        return solve(network);
     }
 
     @Override
     public SolutionStatistics geneticAlgorithm() {
-        return null;
+        var network = baseBuilder()
+                .withGA(200, 100, 10);
+
+        return solve(network);
     }
 
     @Override
     public SolutionStatistics mimic() {
-        return null;
+        throw new UnsupportedOperationException("Not part of the assignment");
     }
 
     private OptNetworkBuilder baseBuilder() {
-        TestTrainSplitFilter testTrainSplit = new TestTrainSplitFilter(percentTrain);
-        testTrainSplit.filter(data);
-        var train = testTrainSplit.getTrainingSet();
-        var test= testTrainSplit.getTestingSet();
-
         return new OptNetworkBuilder()
                 .withActivationFunction(new ScaledExponentialLinearUnit())
                 .withLayers(new int[] {10, 10, 10, 10, 10, 10, outputLayerSize})
                 .withIterations(iterations)
-                .withDataSet(train, test);
+                .withDataSet(trainingSet, testSet);
+    }
+
+    private SolutionStatistics solve(OptNetworkBuilder builder) {
+        long startTime = System.currentTimeMillis();
+        var network = builder.train();
+        long trainTime = System.currentTimeMillis() - startTime;
+
+        double score = score(network);
+        return new SolutionStatistics(null, trainTime, iterations, score);
+    }
+
+    private double score(FeedForwardNetwork network) {
+        double error = 0.0;
+        GradientErrorMeasure measure = new SumOfSquaresError();
+
+        for(int i = 0; i < testSet.size(); ++i) {
+            Instance pattern = testSet.get(i);
+            network.setInputValues(pattern.getData());
+            network.run();
+            Instance output = new Instance(network.getOutputValues());
+            error += measure.value(output, pattern);
+        }
+
+        return error / (double)testSet.size();
     }
 }
